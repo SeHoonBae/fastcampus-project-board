83e3f9153b225610bc022eee1ba43fa594084a4a
SeHoonBae <47914153+SeHoonBae@users.noreply.github.com>
2022-07-16 20:30:10 +0900

    Feature/#8 domain (#8)
    * #7 - erd 벡터 템플릿 추가
    
    * #7 - ERD 그림 완성:Update project-board-erd.svg
    
    * directory and file move
    
    * #7 - 도메인 클래스 설계(게시글, 댓글)
    도메인 정보를 설계해 보았다.
    기초적인 게시글과 댓글 표현이 가능하도록 함
    DB와 연동을 위한 추가적인 표현은 추후에 업데이트 될 수 있음

---
+++ src/main/java/com/fastcampus/projectboard/domain/ArticleComment.java
@@ -0,0 +1,17 @@
+package com.fastcampus.projectboard.domain;
+
+import java.time.LocalDateTime;
+
+public class ArticleComment {
+
+    private Long id;
+    private Article article; // 게시글 아이디
+    private String content; // 본문
+
+    // metadata
+    private LocalDateTime createdAt; // 생성일시
+    private String createdBy; // 생성자
+    private LocalDateTime modifiedAt; // 수정일시
+    private String modifiedBy; // 수정자
+
+}
