8439e8d84fb3b89edbf224d928a86a8685e830e8
SeHoonBae <47914153+SeHoonBae@users.noreply.github.com>
2022-07-22 16:41:15 +0900

    #1 - Querydsl 지원을 이용하여 api 검색 옵션 추가 `QuerydslPredicateExecutor`, `QuerydslBinderCustomizer`를 이용하여 간단하게 개발

--- src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleCommentRepository.java
@@ -1,14 +1,28 @@
 package com.fastcampus.projectboard.repository;
 
 import com.fastcampus.projectboard.domain.ArticleComment;
+import com.fastcampus.projectboard.domain.QArticleComment;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface ArticleCommentRepository extends
         JpaRepository<ArticleComment, Long>,
-        QuerydslPredicateExecutor<ArticleComment> // QuerydslPredicateExecutor를 쓰는것만으로도 ArticleComment에 정의된 필드들에 대한 검색기능이 추가된다.
+        QuerydslPredicateExecutor<ArticleComment>, // QuerydslPredicateExecutor를 쓰는것만으로도 ArticleComment에 정의된 필드들에 대한 검색기능이 추가된다.
+        QuerydslBinderCustomizer<QArticleComment>
 {
 
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root){
+        bindings.excludeUnlistedProperties(true); // 기본값은 false이며 ArticleComment 모든 필드들을 검색하도록 되어 있는 것을 닫는 역할
+        bindings.including(root.content, root.createdAt, root.createdBy);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+
+    }
 }
--- src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -2,6 +2,8 @@ package com.fastcampus.projectboard.repository;
 
 import com.fastcampus.projectboard.domain.Article;
 import com.fastcampus.projectboard.domain.QArticle;
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
@@ -16,6 +18,14 @@ public interface ArticleRepository extends
 {
     @Override
     default void customize(QuerydslBindings bindings, QArticle root){
+        bindings.excludeUnlistedProperties(true); // 기본값은 false이며 Article의 모든 필드들을 검색하도록 되어 있는 것을 닫는 역할
+        bindings.including(root.title, root.content, root.hashtag, root.createdAt, root.createdBy);
+//        bindings.bind(root.content).first(StringExpression::likeIgnoreCase); // like '%s${v}'
+        bindings.bind(root.title).first(StringExpression::containsIgnoreCase); // like '%s${v}%'
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase); // like '%s${v}%'
+        bindings.bind(root.hashtag).first(StringExpression::containsIgnoreCase); // like '%s${v}%'
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
 
     }
 }
